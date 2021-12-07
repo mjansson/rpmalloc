b0575b6dc0d7b64ce615644d672c3715f276d736
David CARLIER <devnexen@gmail.com>
2021-12-07 07:31:37 +0000

    Fix unit test build with clang. (#264)

--- test/test.h
+++ test/test.h
@@ -1,5 +1,11 @@
 
 #pragma once
 
+#if defined(__clang__)
+#if __has_warning("-Wreserved-identifier")
+#pragma clang diagnostic ignored "-Wreserved-identifier"
+#endif
+#endif
+
 extern int
 test_run(int argc, char** argv);
